17b541a2ad8ebc3c587e73589b14c7a464761785
Prasana <prasana.kalanathan@gmail.com>
2019-12-19 11:52:36 +0000

    :smile: First mini blockchain complete
    Implemented a quick blockchain, using a basic hash function.

--- Chapter 2 - Blockchain Lightning version/main.py
+++ Chapter 2 - Blockchain Lightning version/main.py
@@ -1,3 +1,7 @@
+def lightning_hash(data):
+    return data + '*'
+
+
 class Block:
     def __init__(self, data, hash, last_hash):
         self.data = data
@@ -5,9 +9,27 @@ class Block:
         self.last_hash = last_hash
 
 
-foo_block = Block('foo_data', 'foo_hash', 'foo_last_hash')
+class Blockchain:
+    def __init__(self):
+        genesis = Block('gen_data', 'gen_hash', 'gen_last_hash')
+
+        self.chain = [genesis]
+
+    def add_block(self, data):
+        last_hash = self.chain[-1].hash
+        hash = lightning_hash(data + last_hash)
+        block = Block(data, hash, last_hash)
+
+        self.chain.append(block)
 
 
-print(foo_block.data)
-print(foo_block.hash)
-print(foo_block.last_hash)
+foo_blockchain = Blockchain()
+
+foo_blockchain.add_block('one')
+foo_blockchain.add_block('two')
+foo_blockchain.add_block('three')
+
+for block in foo_blockchain.chain:
+    print(block.__dict__)
+
+
