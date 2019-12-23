87a77a535698de7f4e082d0a9a1d3af1dcd8f6d6
Prasana <prasana.kalanathan@outlook.com>
2019-12-23 17:49:03 +0000

    Added a SHA 26 hash function
    Learnt about lambda, map, sorted, json, uft-8

--- block.py
+++ block.py
@@ -1,23 +1,6 @@
 import time
 
-def mine_block(last_block, data):
-    """
-    Mine a block based on the given last_block and data
-    """
-
-    timestamp = time.time_ns()
-    last_hash = last_block.hash
-    hash = f'{timestamp}-{last_hash}'
-
-    return Block(timestamp, last_hash, hash, data)
-
-def genesis():
-    """
-    Generate the genesis block
-    """
-
-    return Block(1, 'genesis_last_hash', 'genesis_hash', [])
-
+from crypto_hash import crypto_hash
 
 class Block:
     """
@@ -35,13 +18,33 @@ class Block:
         f'timestamp: {self.timestamp}, '
         f'last_hash: {self.last_hash}, '
         f'hash: {self.hash}, '
-        f'data: {self.data},)'
+        f'data: {self.data})'
         )
+
+    @staticmethod
+    def mine_block(last_block, data):
+        """
+        Mine a block based on the given last_block and data
+        """
+
+        timestamp = time.time_ns()
+        last_hash = last_block.hash
+        hash = crypto_hash(timestamp, last_hash, data)
+
+        return Block(timestamp, last_hash, hash, data)
+
+    @staticmethod
+    def genesis():
+        """
+        Generate the genesis block
+        """
+
+        return Block(1, 'genesis_last_hash', 'genesis_hash', [])
 
 def main():
 
-    genesis_block = genesis()
-    block = mine_block(genesis_block,'pk')
+    genesis_block = Block.genesis()
+    block = Block.mine_block(genesis_block,'pk')
     print(block)
 
 if __name__ == '__main__':
