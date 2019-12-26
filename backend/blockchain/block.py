07fea7c58e5af648917807e09bf0363d042cb855
Prasana <prasana.kalanathan@outlook.com>
2019-12-26 11:39:47 +0000

    Added dynamic mining rate
    By looking at the differences of consecutive mined block's timestamps

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,6 +1,7 @@
 import time
 
 from backend.util.crypto_hash import crypto_hash
+from backend.config import MINE_RATE
 
 GENESIS_DATA = {
     'timestamp': 1,
@@ -42,13 +43,14 @@ class Block:
         """
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        difficulty = last_block.difficulty
+        difficulty = Block.adjust_difficulty(last_block, timestamp)
         nonce = 0
         hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
         while hash[0:difficulty] != '0' * difficulty:
             nonce += 1
             timestamp = time.time_ns() #ensures the latest time is used
+            difficulty = Block.adjust_difficulty(last_block, timestamp)
             hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
         return Block(timestamp, last_hash, hash, data, difficulty, nonce)
@@ -67,7 +69,22 @@ class Block:
         # )
 
         # Unpacks the GENESIS_DATA dict using **
-        return Block(**GENESIS_DATA) 
+        return Block(**GENESIS_DATA)
+
+    @staticmethod
+    def adjust_difficulty(last_block, new_timestamp):
+        """
+        Calculate the adjusted difficulty according to the MINE_RATE
+        Increase the difficulty for quickly mined blocks
+        Decrease the difficulty for slowly mined blocks
+        """
+        if (new_timestamp - last_block.timestamp) < MINE_RATE:
+            return last_block.difficulty + 1
+        
+        if (last_block.difficulty - 1) > 0: # Ensures difficulty does not go to negative numbers 
+            return last_block.difficulty - 1
+
+        return 1
 
 def main():
 
