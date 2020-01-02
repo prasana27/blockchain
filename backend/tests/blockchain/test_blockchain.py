41ab9fb614b6fa74bfe374691f0bfe437a21897f
Prasana <prasana.kalanathan@outlook.com>
2020-01-02 17:36:26 +0000

    Chain replacement complete with tests

--- backend/tests/blockchain/test_blockchain.py
+++ backend/tests/blockchain/test_blockchain.py
@@ -1,3 +1,5 @@
+import pytest
+
 from backend.blockchain.blockchain import Blockchain
 from backend.blockchain.block import GENESIS_DATA
 
@@ -13,14 +15,43 @@ def test_add_block():
 
     assert blockchain.chain[-1].data == data
 
-def test_is_valid_chain():
-    blockchain = Blockchain()
 
+@pytest.fixture
+def blockchain_three_blocks():
+    blockchain = Blockchain()
     for i in range(3):
         blockchain.add_block(i)
+    
+    return blockchain
 
-    Blockchain.is_valid_chain(blockchain.chain)
+def test_is_valid_chain(blockchain_three_blocks):
+    
+    Blockchain.is_valid_chain(blockchain_three_blocks.chain)
 
+def test_is_valid_chain_bad_genesis(blockchain_three_blocks):
+    blockchain_three_blocks.chain[0].hash = 'evil_hash'
+
+    with pytest.raises(Exception, match='The geneis block must be valid'):
+        Blockchain.is_valid_chain(blockchain_three_blocks.chain)
+
+def test_replace_chain(blockchain_three_blocks):
+    blockchain = Blockchain()
+    blockchain.replace_chain(blockchain_three_blocks.chain)
+
+    assert blockchain.chain == blockchain_three_blocks.chain
+
+def test_replace_chain_not_longer(blockchain_three_blocks):
+    blockchain = Blockchain()
+
+    with pytest.raises(Exception, match = 'Cannot replace. The incoming chain must be longer.'):
+        blockchain_three_blocks.replace_chain(blockchain.chain)
+
+def test_block_chain_bad_chain(blockchain_three_blocks):
+    blockchain = Blockchain()
+    blockchain_three_blocks.chain[1].hash = 'evil_hash'
+    
+    with pytest.raises(Exception, match = 'The incoming chain is invalid'):
+        blockchain.replace_chain(blockchain_three_blocks.chain)
 
 
 
