b491595ecf7fa8e0340ec16ccfc792785b031bed
Prasana <prasana.kalanathan@outlook.com>
2020-01-24 19:29:13 +0000

    Added tests for Wallet class

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -1,8 +1,11 @@
 import uuid
+import json
 
 from backend.config import STARTING_BALANACE
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import hashes
+from cryptography.exceptions import InvalidSignature
 
 class Wallet:
     """
@@ -17,9 +20,42 @@ class Wallet:
         self.private_key = ec.generate_private_key(ec.SECP256K1(), default_backend())
         self.public_key = self.private_key.public_key()
 
+    def sign(self, data):
+        """
+        Generate a signature based on the data using the local private key.
+        """
+        return self.private_key.sign(json.dumps(data).encode('utf-8'), ec.ECDSA(hashes.SHA256()))
+
+    @staticmethod
+    def verify(public_key, data, signature):
+        """
+        Verify a signature based on the original public key and data
+        """
+        try:
+            public_key.verify(
+                signature,
+                json.dumps(data).encode('utf-8'),
+                ec.ECDSA(hashes.SHA256())
+            )
+            return True
+        except InvalidSignature:
+            return False
+
+
 def main():
     wallet = Wallet()
     print(f'wallet.__dict__: {wallet.__dict__}')
+
+    data = {'pk':'don'}
+    signature = wallet.sign(data)
+    print(f'signature: {signature}')
+
+    should_be_valid = Wallet.verify(wallet.public_key, data, signature)
+    print(f'should_be_valid: {should_be_valid}')
+
+    should_be_invalid = Wallet.verify(Wallet().public_key, data, signature)
+    print(f'should_be_invalid: {should_be_invalid}')
+
 
 if __name__ == '__main__':
     main()
